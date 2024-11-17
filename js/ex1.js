6de362dd3b7b79ec04f6e2aa1b7974838d70480b
unknown <you@example.com>
2024-11-17 14:46:29 +0600

    first commit

---
+++ js/ex1.js
@@ -0,0 +1,59 @@
+// document.getElementById("student_message").innerHTML="Hi, my name is Lamia, I'm 21 years old and I'm learning Javascript";
+
+
+// var num1 = 101;
+// var num2 = 7;
+// var division = num1 / num2;
+// console.log("division.toFixed(4)"); 
+// document.getElementById("result").innerHTML=division.toFixed(4);
+
+
+
+// var phone1 = "988866552";
+// var phone2 = "99087612366";
+// var phone3 = 876543123;
+
+
+// if( phone1.length == "9") {
+//         console.log(phone1+ "valid");
+//     } else {
+//         console.log(phone1 + "invalid");
+//     }
+
+
+// if( phone2 === "9") {
+//             console.log(phone2.length + "valid");
+//         } else {
+//             console.log(phone2 + "invalid");
+//         }
+
+// if( phone3 === "9") {
+//             console.log(phone3.length + "valid");
+//             } else {
+//             console.log(phone3 + "invalid");
+//             }
+
+// var quantity = "25";
+// console.log(quantity += quantity);
+
+
+// var number = 6;
+// console.log( (7+5) / number + 2 );
+
+
+// var pressure;
+// console.log(pressure);
+
+
+// var temperature = null;
+// console.log(temperature);
+
+
+// var pressure;
+// console.log(typeof pressure);
+
+
+// var temperature = null;
+// console.log(typeof temperature);
+
+
---
+++ js/scripts.js
@@ -0,0 +1,230 @@
+// This function is to show hello world
+
+
+/*\
+ 1, dfdff
+ 3 dfd
+4. multiline comments
+
+*/
+//dfdfd
+// var fullName="Lamia Akter";
+// console.log(fullName);
+
+// var firstName="Birthday";
+// var birthYear = "04-03-2003"; 
+// console.log(firstName+" "+ birthYear);
+
+// var num1 = 200;
+// var num2 = 47;
+// var subtraction = (num1 - num2);
+// console.log(subtraction)
+
+// var sum = (num1 + num2);
+// console.log(num1 + num2)
+
+// var count=6;
+// var randomNumber= Math.random();
+// console.log("random number" + randomNumber);
+// var fixedNumber= randomNumber.toFixed(2);
+// console.log("This is nweNumber" + fixedNumber);
+
+// var count= 6;
+// count/= 4;
+// console.log(count);
+
+// var count1="40.05";
+// var count2="20";
+// console.log( parseFloat(count1) +  parseInt(count2));
+
+
+
+// var birthday ="1st october";
+// var currentDate="2nd october";
+
+
+// if( birthday == "2nd october") {
+//     console.log("Happy Birthday");
+// } else {
+//     console.log("sorry,today is not your birthday");
+// }
+
+
+//  var birthday ="4th March";
+//  var currentDate="4th March";
+
+
+//  if( birthday == currentDate) {
+//      console.log("Happy Birthday");
+//  } else {
+//      console.log("sorry,today is not your birthday");
+//  }
+
+
+// function minus(num1,num2) {
+//     var result=(num1-num2);
+//     console.log (result);
+
+//     alert(result);
+// }
+// minus(1024,128);
+
+
+// function divisionResult(number1,number2) {
+//     var result=number1/number2;
+//     return result;  
+// }
+// var divisionResult= divisionNumbers(873,321);
+// alert(divisionResult);
+
+
+// var divisionResult1= divisionNumbers=(378,23);
+// var divisionResult2=divisionNumbers2=(76,65);
+// var divisionResult3=divisionNumbers3=(30,10);
+
+// alert(divisionNumbers,divisionNumbers2,divisionNumbers3);
+
+
+
+function division(c) {
+
+    var result=(9*c/5) +32
+    return result;
+    
+}
+var result=division(38);
+console.log(result);
+document.getElementById("fahr_1").innerHTML=result;
+
+
+
+function division(c) {
+
+    var result=(9*c/5) +32
+    return result;
+    
+}
+var result=division(24);
+console.log(result);
+document.getElementById("fahr_2").innerHTML=result;
+
+
+function division(c) {
+
+    var result=(9*c/5) +32
+    return result;
+    
+}
+var result=division(-1);
+console.log(result);
+document.getElementById("fahr_3").innerHTML=result;
+
+
+var classification = [ "John Hill" , "Mary Jane", "Gary Vee", "Paricia Mills", "Helen Hall", "Paul Green" ]; 
+classification.shift();
+console.log(classification);
+
+var classification = [ "Mary Jane", "Gary Vee", "Paricia Mills", "Helen Hall", "Paul Green" ]; 
+classification.shift();
+console.log(classification);
+
+var classification = [ "Mary Jane", "Gary Vee", "Paricia Mills", "Helen Hall", "Paul Green" ]; 
+classification.shift();
+console.log(classification);
+document.getElementById("best_students").innerHTML=classification
+
+
+var course = {
+    'title': "Learn to Code in Python 3",
+    'categories': ['programming', 'technology', 'python'],
+    '5_stars_reviews': 420,
+    '4_stars_reviews': 80,
+    '3_stars_reviews': 33,
+    '2_stars_reviews': 20,
+    '1_stars_reviews': 4
+}
+console.log(course.title);
+document.getElementById("course_title").innerHTML=course.title
+
+console.log(course['categories'][0]);
+document.getElementById("main_category").innerHTML=course.title
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("click-me").onclick = function() {
+//     var firstName="Lamia Akter";
+//     alart("firstName");
+// };
+
+
+// document.getElementById("click-me").onclick = function() {
+//     alert('i love bangladesh');
+// };
+
+
+// document.getElementById("hover-me").onmouseover = function() {
+//     alert('hello,World');
+// };
+
+// document.getElementById("leave-me").onmouseout = function() {
+//     alert('hello,bangladesh');
+// };
+
+// document.getElementById("look-me").onclick = function() {
+//     alert('bangladesh');
+// };
+
+
+// var clickButton = document.getElementById("color_button");
+// // var showButton = document.getElementById("hide");
+
+// clickButton.onclick = function () {
+//     document.getElementById('box2').classList.toggle("show")
+// }
+
+// showButton.onclick = function () {
+//     document.getElementById("box2").classList.remove("show")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
